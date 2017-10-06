25ce1e254c13b74718f04ae95db047c5bbebd19d
naufaldi <naufaldi.rafif@gmail.com>
2017-10-06 09:23:12 +0700

    js jump iteration

---
+++ jump_js/iteration_method/for_each.js
@@ -0,0 +1,24 @@
+var arr , i,num,total;
+
+aar = [80,20,40,30,14,24];
+total = 0;
+
+for (i = 0; i < arr.lenth ; i= 1 +i){
+    num = arr[i];
+    total = total + num;
+}
+
+alert("Total is : "+total);
+
+var total2,arr2;
+arrr2 = [20,20,30,40,15,25];
+total = 0;
+
+array.forEach(function(num) {
+ total = total +num;   
+});
+
+alert("Total is "+ total);
+//Traditionally, when you wanted to work with all the items in an array, you’d loop
+//over it. Though we’ll be covering loops later, it’s important to show you how it used
+//to be done so that you understand why the forEach method is handy.
---
+++ jump_js/iteration_method/map.js
@@ -0,0 +1,6 @@
+var arr = [1,4,5,56,2,62,];
+var squared;
+
+squared = arr.map((num) => {
+    return (num * num);
+}); //square is [1,16,25,...,4,...]
