acc3f414caa4b6d66befc6f8fb68700c3b7e6fd4
naufaldi <naufaldi.rafif@gmail.com>
2019-01-02 02:01:50 +0700

    spreadoperator

--- ES6/forOfLoop.js
+++ ES6/forOfLoop.js
@@ -1,2 +1,17 @@
-let income = [62000,67000,75000];
+let incomes = [62000,67000,75000];
 let total =0;
+
+for(const income of incomes){
+  console.log(income);
+}
+// artinya setiap array index income dari incomes akan dicetak
+//62000
+//67000
+//75000
+
+
+let fullName = "Naufaldi Rafif";
+for(const char of fullName){
+  console.log(char);
+}
+//cetak perbagian huruf
---
+++ ES6/spreadOperator.js
@@ -0,0 +1,5 @@
+let example2 = [1,2,3,4,5,6];
+let example1 = [...example2];
+console.log(example2);
+console.log(example1);
+
